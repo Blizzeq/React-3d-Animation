706aa769f9061dc9fb4f4c8fcb88a69fece4b4e9
Jakub Krasuski <kubamax4@gmail.com>
2023-03-29 20:15:43 +0200

    Added Sound && DisplaySection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './Components/Navbar.jsx';
 import Jumbotron from "./Components/Jumbotron.jsx";
 import Sound from "./Components/Sound.jsx";
+import DisplaySection from "./Components/DisplaySection.jsx";
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
         <Navbar />
         <Jumbotron />
         <Sound />
+        <DisplaySection />
     </div>
   );
 }
