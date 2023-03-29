a168c70cd2919cf23f4162d75032d196c08f43ca
Jakub Krasuski <kubamax4@gmail.com>
2023-03-29 21:44:37 +0200

    WebgiViewer problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './Components/Navbar.jsx';
 import Jumbotron from "./Components/Jumbotron.jsx";
 import Sound from "./Components/Sound.jsx";
 import DisplaySection from "./Components/DisplaySection.jsx";
+import WebGiViewer from "./Components/WebGiViewer.jsx";
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
         <Jumbotron />
         <Sound />
         <DisplaySection />
+        <WebGiViewer />
     </div>
   );
 }
