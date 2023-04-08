fadb4478b9a87a3c5b54992e36cd83a9f6494043
Jakub Krasuski <kubamax4@gmail.com>
2023-04-08 19:27:04 +0200

    "Final commit: completed project"

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import Jumbotron from "./Components/Jumbotron.jsx";
 import Sound from "./Components/Sound.jsx";
 import DisplaySection from "./Components/DisplaySection.jsx";
 import WebGiViewer from "./Components/WebGiViewer.jsx";
+import {useRef} from "react";
+import Loader from "./Components/Loader.jsx";
 function App() {
+
+    const webGiViewerRef = useRef(null);
+    const contentRef = useRef(null);
+
+    const handlePreview = () => {
+        webGiViewerRef.current.triggerPreview();
+    }
 
   return (
     <div className="App">
-        <Navbar />
-        <Jumbotron />
-        <Sound />
-        <DisplaySection />
-        <WebGiViewer />
+        <Loader />
+        <div ref={contentRef} id={'content'}>
+            <Navbar />
+            <Jumbotron />
+            <Sound />
+            <DisplaySection triggerPreview={handlePreview}/>
+        </div>
+        <WebGiViewer ref={webGiViewerRef} contentRef={contentRef}/>
     </div>
   );
 }
